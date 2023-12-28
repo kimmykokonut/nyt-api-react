787f7bd75f10aac5ab6d2f4531af05e8079cb6c4
Kim Robinson <kimmykokonut@gmail.com>
2023-12-28 14:45:02 -0800

    pass default test reducer

---
+++ src/__tests__/reducers/top-stories-reducer.test.js
@@ -0,0 +1,17 @@
+import topStoriesReducer from '../../reducers/top-stories-reducer';
+
+describe('topStoriesReducer', () => {
+  const initialState = {
+    isLoaded: false,
+    topStories: [],
+    error: null
+  };
+  test('should successfully throw a new error if a non-matching action type is passed into it', () => {
+    expect(
+      () => {
+        topStoriesReducer(initialState, {type: null })
+      }
+    ).toThrowError("There is no action matching null.");
+  });
+  
+});
