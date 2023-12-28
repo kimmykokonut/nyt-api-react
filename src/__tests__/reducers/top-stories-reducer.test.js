21280b610155125f4b51a7671cd9d4affe170d81
Kim Robinson <kimmykokonut@gmail.com>
2023-12-28 14:52:27 -0800

    success reducer test pass

--- src/__tests__/reducers/top-stories-reducer.test.js
+++ src/__tests__/reducers/top-stories-reducer.test.js
@@ -1,6 +1,9 @@
 import topStoriesReducer from '../../reducers/top-stories-reducer';
+import * as c from './../../actions/ActionTypes';
 
 describe('topStoriesReducer', () => {
+  let action; //declare action as variable
+
   const initialState = {
     isLoaded: false,
     topStories: [],
@@ -13,5 +16,18 @@ describe('topStoriesReducer', () => {
       }
     ).toThrowError("There is no action matching null.");
   });
-  
+
+  test('successfully getting top stories should change isLoaded to true and update topStories', () => {
+    const topStories = 'An article';
+    action = {
+      type: c.GET_TOP_STORIES_SUCCESS,
+      topStories
+    };
+    expect(topStoriesReducer(initialState, action)).toEqual({
+      isLoaded: true,
+      topStories: 'An article',
+      error: null
+    });
+  });
+
 });
